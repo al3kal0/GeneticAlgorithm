4d8519f05b764a2466076d300fb0b1d7dcdacd9c
al3kal0 <70112686+al3kal0@users.noreply.github.com>
2022-09-08 22:22:33 -0400

    Update working-generic-interfaces.zig

--- working-generic-interfaces.zig
+++ working-generic-interfaces.zig
@@ -62,50 +62,99 @@ pub fn Step(comptime T: type) type {
 // end of file Step.zig
 
 // main.zig
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Step = @import("Step.zig").Step;
+const test_allocator = std.testing.allocator;
 
 pub fn Generic(comptime T: type) type {
     return struct {
         val0: T,
         val1: T,
+        buffer: []T,
         term_condition: bool,
+        allocator: Allocator,
         
         const Self = @This();
         
-        pub fn create() !Self {
+        pub fn create(allocator: Allocator) !Self {
             return .{
                 .val0 = undefined,
                 .val1 = undefined,
-                .term_condition = false,               
+                .buffer = try allocator.alloc(T, 200),
+                .term_condition = false,              
+                .allocator = allocator, 
             };
+        }
+        
+        pub fn destroy(self: *Self) void {
+            self.allocator.free(self.buffer);
         }
         
         pub fn initialize(alg: *Self, init: *Step(T)) void {
             init.exec(alg);
         }
+        
+        pub fn run(alg: *Self, init: *Step(T), term: *Step(T)) void {
+            init.exec(alg);
+            while(!alg.term_condition) {
+               term.exec(alg); 
+            }
+        }
     };
 }
 
-const TermStep = struct {
-    iteration: u32,
-    max_iterations: u32,
-    
-    fn term(self: *TermStep, alg: *Generic(f32)) void {
-        self.iteration += 1;
-        alg.term_condition = self.iteration >= self.max_iterations;
-    }
-    
-    pub fn step(self: *TermStep) Step(f32) {
-        return Step(f32).init(self, term);
-    }
-        
-};
+
 
 test "test interface" {
-    var generic = try Generic(f32).create();
-    var term_step = TermStep{ .iteration = 0, .max_iterations = 30 };
-    var term = term_step.step();
     
-    generic.initialize(&term);
+    const Experiment = struct {
+        time: f32,
+        temp: f32,       
+    };
+    
+    const InitStep = struct {
+        val: u8,
+        
+        const Self = @This();
+        
+        fn init(self: *Self, alg: *Generic(Experiment)) void {
+            _ = self;
+            for(alg.buffer) |*chrom| {
+                chrom.* = Experiment{ .time = 56.4, .temp = 102.8 };
+            }
+        }
+        
+        pub fn step(self: *Self) Step(Experiment) {
+            return Step(Experiment).init(self, init);
+        }
+    };
+    
+    const TermStep = struct {
+        iteration: u32,
+        max_iterations: u32,
+        
+        const Self = @This();
+        
+        fn term(self: *Self, alg: *Generic(Experiment)) void {
+            self.iteration += 1;
+            alg.term_condition = self.iteration >= self.max_iterations;
+        }
+        
+        pub fn step(self: *Self) Step(Experiment) {
+            return Step(Experiment).init(self, term);
+        }
+            
+    };
+    
+    var generic = try Generic(Experiment).create(test_allocator);
+    defer generic.destroy();
+    var init_step = InitStep{ .val = undefined };
+    var term_step = TermStep{ .iteration = 0, .max_iterations = 30 };
+    var _term = term_step.step();
+    var _init = init_step.step();
+    
+    generic.run(&_init, &_term);
     
 }
 
